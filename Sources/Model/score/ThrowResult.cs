c5bf7dab879b4973a9476bb76aaa9bad6a7a134d
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-09-13 17:55:36 +0200

    [#16] Added enum ThrowResult

---
+++ Sources/Model/score/ThrowResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.score
+{
+    internal enum ThrowResult
+    {
+        STRIKE,
+        SPAIR,
+        ZERO,
+        ONE,
+        TWO,
+        TREE,
+        FOUR,
+        FIVE,
+        SIX,
+        SEVEN,
+        EIGHT,
+        NINE
+    }
+}
