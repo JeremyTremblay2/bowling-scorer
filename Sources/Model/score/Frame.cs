8824f34705949d53034d83ffd0d343d7623a7c07
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-09-16 10:48:12 +0200

    [#14] Frame.cs added

---
+++ Sources/Model/score/Frame.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.score
+{
+    public class Frame
+    {
+        private ThrowResult[] throwResults;
+        public System.Collections.ObjectModel.ReadOnlyCollection<ThrowResult> ThrowResults
+        {
+            get;
+            private set;
+        }
+        private int scoreValue;
+        public int ScoreValue
+        {
+            get { return scoreValue; }
+            set {
+                if (value < 0) scoreValue = 0;
+                else scoreValue = value;
+            }
+        }
+        public Frame(int numberOfThrow)
+        {
+            throwResults = new ThrowResult[numberOfThrow];
+            ThrowResults = new System.Collections.ObjectModel.ReadOnlyCollection<ThrowResult>(throwResults);
+        }
+
+        public void AddThrow(int index, ThrowResult throwResult) 
+        {
+            throwResults[index] = throwResult;
+        }
+
+    }
+}
