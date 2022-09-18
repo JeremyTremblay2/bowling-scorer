e138caf3db28201d45e050b48b96f5c8185f1da2
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-09-18 21:22:04 +0200

    [#6] Added interface IScoreCalculator and her documentation

---
+++ Sources/Model/score/IScoreCalculator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.score
+{
+    /// <summary>
+    /// ScoreCalculator is used to compute the results in frames
+    /// </summary>
+    public interface IScoreCalculator
+    {
+        /// <summary>
+        /// Calculate the total score of a Frame list
+        /// </summary>
+        /// <param name="frames">List of frames</param>
+        /// <returns>The total score calculated</returns>
+        public int CalculateScore(List<Frame> frames);
+
+        /// <summary>
+        /// Update the score of the last Frame
+        /// </summary>
+        /// <param name="frames">List of frames</param>
+        public void UpdateLastFrame(List<Frame> frames);
+        /// <summary>
+        /// Update the score from a specific Frame
+        /// </summary>
+        /// <param name="index">Index of the frame to update</param>
+        /// <param name="frames">List of frames</param>
+        public void UpdateFromFrame(int index, List<Frame> frames);
+    }
+}
