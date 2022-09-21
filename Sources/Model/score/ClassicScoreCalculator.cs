6504f922de51e4b271b5e0ffd921519248977f1d
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-09-21 19:42:33 +0200

    [#7] Calculator - home version

--- Sources/Model/score/ClassicScoreCalculator.cs
+++ Sources/Model/score/ClassicScoreCalculator.cs
@@ -10,41 +10,126 @@ namespace Model.score
 {
     public class ClassicScoreCalculator : IScoreCalculator
     {
-        public int CalculateScore(List<Frame> frames)
+        public int CalculateScore(List<AFrame> frames)
         {
             int total = 0;
-            foreach (Frame frame in frames)
+            foreach (AFrame frame in frames)
             {
                 total = total + frame.ScoreValue;
             }
             return total;
         }
 
-        public void UpdateFromFrame(int index, List<Frame> frames)
+        public void UpdateFromFrame(int index, List<AFrame> frames)
         {
-            ComputeOneFrame(index, frames);
+            AFrame frame0 = frames[index];
+            if (frame0 == null) return;
+            if (frame0 is ClassicFrame)
+            {
+                ClassicFrame classic0 = (ClassicFrame)frame0;
+                if (index + 1 == 8)
+                {
+                    //la prochaine frame est une last
+                }
+                else
+                {
+                    ComputeClassicFrame(classic0, frames);
+                    ComputeOlderClassicFrames(index, frames);
+                }
+            }
+            else if (frame0 is ClassicLastFrame)
+            {
+                ComputeClassicLastFrame((ClassicLastFrame)frame0);
+            }
         }
 
-        public void UpdateLastFrame(List<Frame> frames)
+        public void UpdateLastFrame(List<AFrame> frames)
         {
             int index = frames.Count - 1;
-            ComputeOneFrame(index, frames);
+            AFrame frame0 = frames[index];
+            if (frame0 == null) return;
+            if (frame0 is ClassicFrame)
+            {
+                ClassicFrame classic0 = (ClassicFrame)frame0;
+                ComputeClassicFrame(classic0, frames);
+            }
+            else if (frame0 is ClassicLastFrame)
+            {
+                ComputeClassicLastFrame((ClassicLastFrame)frame0);
+            }
         }
 
-        private void ComputeOneFrame(int selectedFrameIdx, List<Frame> scoreBoard)
+        private void ComputeOlderClassicFrames(int index, List<AFrame> frames)
         {
-            Frame frame = scoreBoard[selectedFrameIdx];
-            int calculatedScore = 0;
-            if (frame.isStrike() || frame.isSpair())
+            ComputeClassicFrame((ClassicFrame)frames[index - 1], frames);
+            ComputeClassicFrame((ClassicFrame)frames[index - 2], frames);
+        }
+
+        private void ComputeClassicLastFrame(ClassicLastFrame frame0)
+        {
+            int computedScore = 0;
+            if (frame0.ThrowResults[0] == ThrowResult.STRIKE)
             {
-                Frame nextFrame = scoreBoard[selectedFrameIdx + 1];
+                computedScore = computedScore + 10;
+                if (frame0.ThrowResults[1] == ThrowResult.STRIKE)
+                {
+                    computedScore = computedScore + 10;
+                    if (frame0.ThrowResults[2] == ThrowResult.STRIKE)
+                    {
+                        computedScore = computedScore + 10;
+                    }
+                }
+                else
+                {
+                    if (frame0.ThrowResults[2] == ThrowResult.SPAIR)
+                    {
+                        computedScore = computedScore + 10;
+                    }
+                    else
+                    {
+                        computedScore = computedScore + ThrowResultExtension.ToInt(frame0.ThrowResults[1])
+                                                      + ThrowResultExtension.ToInt(frame0.ThrowResults[2]);
+                    }
+                }
+            }
+            else
+            {
+                if (frame0.ThrowResults[1] == ThrowResult.SPAIR)
+                {
+                    computedScore = computedScore + 10;
+                    if (frame0.ThrowResults[2] == ThrowResult.STRIKE)
+                    {
+                        computedScore = computedScore + 10;
+                    }
+                    else
+                    {
+                        computedScore = computedScore + ThrowResultExtension.ToInt(frame0.ThrowResults[2]);
+                    }
+                }
+                else
+                {
+                    computedScore = computedScore + ThrowResultExtension.ToInt(frame0.ThrowResults[0])
+                                                  + ThrowResultExtension.ToInt(frame0.ThrowResults[0]);
+                }
+            }
+            frame0.ScoreValue = computedScore;
+        }
+
+        private void ComputeClassicFrame(ClassicFrame frame0, List<AFrame> scoreBoard)
+        {
+            int calculatedScore = 0;
+            int selectedFrameIdx = scoreBoard.IndexOf(frame0);
+            if (selectedFrameIdx == -1) throw new ArgumentException("The given frame is not in the given list of AFrames");
+            if (frame0.isStrike() || frame0.isSpair())
+            {
+                AFrame nextFrame = scoreBoard[selectedFrameIdx + 1];
                 if (nextFrame == null) throw new MissingFrameException();
                 calculatedScore = 10;
-                if (frame.isStrike())
+                if (frame0.isStrike())
                 {
                     if (nextFrame.isStrike())
                     {
-                        Frame nextNextFrame = scoreBoard[selectedFrameIdx + 2];
+                        AFrame nextNextFrame = scoreBoard[selectedFrameIdx + 2];
                         if (nextNextFrame == null) throw new MissingFrameException();
                         calculatedScore = calculatedScore + 10;
                         if (nextNextFrame.isStrike())
@@ -66,7 +151,7 @@ namespace Model.score
                                                           + ThrowResultExtension.ToInt(nextFrame.ThrowResults[1]);
                     }
                 }
-                else if (frame.isSpair())
+                else if (frame0.isSpair())
                 {
                     if (nextFrame.isStrike())
                     {
@@ -80,9 +165,9 @@ namespace Model.score
             }
             else
             {
-                calculatedScore = ThrowResultExtension.ToInt(frame.ThrowResults[0]) + ThrowResultExtension.ToInt(frame.ThrowResults[1]);
+                calculatedScore = ThrowResultExtension.ToInt(frame0.ThrowResults[0]) + ThrowResultExtension.ToInt(frame0.ThrowResults[1]);
             }
-            frame.ScoreValue = calculatedScore;
+            frame0.ScoreValue = calculatedScore;
         }
     }
 }
