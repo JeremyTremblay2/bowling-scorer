1f85ce574e4e586015df5d7201a68d688b7b065f
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-09-24 13:50:12 +0200

    [#31] Added test covering on A_Frame

--- Sources/Tests/UnitTests/score/AFrame_UT.cs
+++ Sources/Tests/UnitTests/score/AFrame_UT.cs
@@ -12,14 +12,87 @@ namespace UnitTests.score
     {
         // Use MOCK (library for instantiate Abstract class but not realy, test the constructor's method
 
-        [Theory]
-        [InlineData(false, 1, 2)]
-        public void Test_Constructor(bool throwException, int frameNumberLabel, int nbThrows)
+        /// <summary>
+        /// Class for testing directly the implemented methods of AFrame
+        /// </summary>
+        private class BadFrame : AFrame
         {
-            if (throwException)
+            public BadFrame(int frameNumberLabel, int nbThrows) : base(frameNumberLabel, nbThrows)
             {
-                Assert.Throws<ArgumentException>(() => {  });
             }
+        }
+        [Fact]
+        public void Test_Constructor()
+        {
+           Assert.Throws<ArgumentException>(() => { new BadFrame(1, -1); });
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(0, 1)]
+        [InlineData(-1, 1)]
+        public void Test_FrameNumberLabelProperty(int number, int exceptedLabel)
+        {
+            AFrame frame = new BadFrame(number, 1);
+            Assert.Equal(exceptedLabel, frame.FrameNumberLabel);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-1, 0)]
+        public void Test_FrameScoreValueProperty(int value, int exceptedValue)
+        {
+            AFrame frame = new BadFrame(1, 1);
+            frame.ScoreValue = value;
+            Assert.Equal(exceptedValue, frame.ScoreValue);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(0, 0)]
+        [InlineData(-1, 0)]
+        public void Test_FrameCumulativeScoreProperty(int cumulScore, int exceptedCumulScore)
+        {
+            AFrame frame = new BadFrame(1, 1);
+            frame.CumulativeScore = cumulScore;
+            Assert.Equal(exceptedCumulScore, frame.CumulativeScore);
+        }
+
+        public static IEnumerable<object[]> Data_Equals()
+        {
+            yield return new object[]
+            {
+                false,
+                null
+            };
+            yield return new object[]
+            {
+                false,
+                new Object()
+            };
+            yield return new object[]
+            {
+                true,
+                new BadFrame(1, 1)
+            };
+        }
+
+        [Theory]
+        [MemberData(nameof(Data_Equals))]
+        public void Test_Equals(bool exceptedResult, Object frame2)
+        {
+            AFrame frame1 = new BadFrame(1, 1);
+            Assert.Equal(exceptedResult, frame1.Equals(frame2));
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        public void Test_GetHashCode(int labelNumber, int hashResult)
+        {
+            AFrame frame = new BadFrame(labelNumber, 2);
+            Assert.Equal(hashResult, frame.GetHashCode());
         }
     }
 }
