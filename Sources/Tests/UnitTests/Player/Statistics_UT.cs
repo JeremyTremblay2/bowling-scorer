8d80181aa64e15f79bd41a89a2dbc0863fe399d7
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-05 22:23:25 +0200

    [#41] Fix conflicts

--- Sources/Tests/UnitTests/Player/Statistics_UT.cs
+++ Sources/Tests/UnitTests/Player/Statistics_UT.cs
@@ -1,10 +1,4 @@
 ﻿using Model.Player;
-using Model.score;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace UnitTests.Player
