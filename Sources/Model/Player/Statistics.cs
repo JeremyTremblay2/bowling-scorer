fca95b9d8f5ec7ae6cb2d12c2ee727eb040f1e50
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-05 19:17:56 +0200

    [#28] Add documentation for the Statistics class

--- Sources/Model/Player/Statistics.cs
+++ Sources/Model/Player/Statistics.cs
@@ -8,36 +8,71 @@ using System.Threading.Tasks;
 
 namespace Model.Player
 {
+    /// <summary>
+    /// Statistics represent various data collected through different games of bowling and are intrinsic to a particular player.
+    /// We can for example cite the average score, the number of games won or the best result. 
+    /// This class is therefore responsible for encapsulating this data.
+    /// </summary>
     public class Statistics : IEquatable<Statistics>
     {
         private readonly IDictionary<ThrowResult, int> throwResults;
         private readonly IList<int> gamesID;
         private readonly IList<int> scores;
 
+        /// <summary>
+        /// Represents the number of total wins by a player.
+        /// </summary>
         public int NumberOfVictory { get; private set; }
-        
+
+        /// <summary>
+        /// Represents the total number of games lost by a player.
+        /// </summary>
         public int NumberOfDefeat { get; private set; }
 
+        /// <summary>
+        /// Represents the total number of games a player has participated in.
+        /// </summary>
         public int NumberOfGames { get; private set; }
 
+        /// <summary>
+        /// Represents the player's best score.
+        /// </summary>
         public int BestScore { get; private set; }
 
+        /// <summary>
+        /// Contains all the scores achieved by the player in each game played.
+        /// </summary>
         public ReadOnlyCollection<int> Scores { get; private set; }
 
+        /// <summary>
+        /// Contains the frequency of the number of pins that fall with each throw of the player.
+        /// </summary>
         public ReadOnlyDictionary<ThrowResult, int> ThrowResults { get; private set; }
 
+        /// <summary>
+        /// Contains each game ID for each game played by the player.
+        /// </summary>
         public ReadOnlyCollection<int> GamesID { get; private set; }
 
+        /// <summary>
+        /// Returns the number of average pins knocked down in a player's throw.
+        /// </summary>
         public double MediumThrow
         {
             get => throwResults.Values.Average();
         }
 
+        /// <summary>
+        /// Returns the average score achieved by the player over all of his games.
+        /// </summary>
         public double MediumScore
         {
             get => Scores.Average();
         }
 
+        /// <summary>
+        /// Create some new statistics.
+        /// </summary>
         public Statistics()
         {
             NumberOfVictory = NumberOfDefeat = BestScore = 0;
@@ -49,11 +84,19 @@ namespace Model.Player
             Scores = new ReadOnlyCollection<int>(scores);
         }
 
+        /// <summary>
+        /// Add a throw result to the player's statistics.
+        /// </summary>
+        /// <param name="throwResult">The throw result of the player.</param>
         public void AddThrowResult(ThrowResult throwResult)
         {
             throwResults.Add(throwResult, 1);
         }
 
+        /// <summary>
+        /// Adds a game played by the player and adds his results.
+        /// </summary>
+        /// <param name="gameID"></param>
         public void AddGame(int gameID)
         {
             if (!gamesID.Contains(gameID))
@@ -63,12 +106,21 @@ namespace Model.Player
             // Implements logic here to add scores and stats.
         }
 
+        /// <summary>
+        /// Deletes a game and all scores related to it.
+        /// </summary>
+        /// <param name="gameID"></param>
         public void RemoveGame(int gameID)
         {
             gamesID.Remove(gameID);
             // Implements logic here to remove scores and stats.
         }
 
+        /// <summary>
+        /// Determines whether the two object instances are equal.
+        /// </summary>
+        /// <param name="other">The object to compare with the actual object.</param>
+        /// <returns>True if the specified object is equal to the current object; otherwise, False.</returns>
         public override bool Equals(Object other)
         {
             if (other == null) return false;
@@ -77,17 +129,30 @@ namespace Model.Player
             return Equals((ThrowResult) other);
         }
 
+        /// <summary>
+        /// Determines whether the two object instances are equal.
+        /// </summary>
+        /// <param name="other">The statistics to compare with the actual stats.</param>
+        /// <returns>True if the specified object is equal to the current object; otherwise, False.</returns>
         public bool Equals(Statistics other)
         {
             return other != null
                 && other.Scores.SequenceEqual(Scores);
         }
 
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
             return HashCode.Combine(Scores);
         }
 
+        /// <summary>
+        /// Returns a string that represents the current object.
+        /// </summary>
+        /// <returns>A string that represents the current object.</returns>
         public override string ToString()
         {
             StringBuilder builder = new();
@@ -98,6 +163,10 @@ namespace Model.Player
             return builder.ToString();
         }
 
+        /// <summary>
+        /// Adds a score to the results.
+        /// </summary>
+        /// <param name="score">The score to be added.</param>
         private void AddScore(int score)
         {
             if (score > BestScore)
