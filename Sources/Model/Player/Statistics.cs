c57afb7af8a1ee19b3022eb574594bf8ccfbeb7f
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-05 22:16:19 +0200

    [#41] Documentation added

--- Sources/Model/Player/Statistics.cs
+++ Sources/Model/Player/Statistics.cs
@@ -178,29 +178,68 @@ namespace Model.Player
             scores.Add(score);
         }
 
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates 
+        /// whether the current instance precedes, follows, or occurs in the same position in the sort order as 
+        /// the other object.
+        /// </summary>
+        /// <param name="obj">The object to compare to this.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
+        /// <exception cref="ArgumentException">If obj is not Statistics.</exception>
         int IComparable.CompareTo(object obj)
         {
-            if (!(obj is Statistics))
+            if (obj is not Statistics)
             {
                 throw new ArgumentException("The argument is not a statistic.", nameof(obj));
             }
             return CompareTo((Statistics) obj);
         }
 
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates 
+        /// whether the current instance precedes, follows, or occurs in the same position in the sort order as 
+        /// the other object.
+        /// </summary>
+        /// <param name="other">The others statistics to compare to this.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
         public int CompareTo(Statistics other)
         {
             return BestScore.CompareTo(other.BestScore);
         }
 
+        /// <summary>
+        /// Returns true if its left-hand operand is less than its right-hand operand, false otherwise.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
         public static bool operator <(Statistics left, Statistics right)
             => left.CompareTo(right) < 0;
 
+        /// <summary>
+        /// Returns true if its left-hand operand is less than or equal to its right-hand operand, false otherwise.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
         public static bool operator <=(Statistics left, Statistics right)
             => left.CompareTo(right) <= 0;
 
+        /// <summary>
+        /// Returns true if its left-hand operand is greater than its right-hand operand, false otherwise.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
         public static bool operator >(Statistics left, Statistics right)
             => left.CompareTo(right) > 0;
 
+        /// <summary>
+        /// Returns true if its left-hand operand is greater than or equal to its right-hand operand, false otherwise.
+        /// </summary>
+        /// <param name="left">The first object to compare.</param>
+        /// <param name="right">The second object to compare.</param>
+        /// <returns>An integer indicating the result of the comparison.</returns>
         public static bool operator >=(Statistics left, Statistics right)
             => left.CompareTo(right) >= 0;
     }
