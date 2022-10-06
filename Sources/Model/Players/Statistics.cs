9747c8975baa25fd920a6a6a25968c8e3e11de25
Jérémy TREMBLAY <jetremblay>
2022-10-06 08:51:34 +0200

    [#29] Refactor conflicting namespaces

--- Sources/Model/Players/Statistics.cs
+++ Sources/Model/Players/Statistics.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Model.Player
+namespace Model.Players
 {
     /// <summary>
     /// Statistics represent various data collected through different games of bowling and are intrinsic to a particular player.
