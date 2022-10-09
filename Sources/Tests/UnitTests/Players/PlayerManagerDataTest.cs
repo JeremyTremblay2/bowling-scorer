7037a446b6cea156cede41cbf9fd8909fa713c95
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-09 22:38:59 +0200

    [#30] Add a Data class for the tests.

---
+++ Sources/Tests/UnitTests/Players/PlayerManagerDataTest.cs
@@ -0,0 +1,90 @@
+﻿using Model.Players;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Players
+{
+    public class PlayerManagerDataTest
+    {
+        public static IEnumerable<object[]> Data_AddPlayersToManager()
+        {
+            yield return new object[] {
+                true,
+                new Player[]
+                {
+                    new Player(1, "Baptiste", "baptisteImage"),
+                    new Player(2, "Elliot", "elliotImage"),
+                    new Player(4, "Harvey", "harveyImage"),
+                    new Player(7, "Lewis", "lewisImage"),
+                    new Player(9, "Haley", "haleyImage"),
+                },
+                new PlayerManager(
+                    new Player(1, "Baptiste", "baptisteImage"),
+                    new Player(2, "Elliot", "elliotImage"),
+                    new Player(7, "Lewis", "lewisImage"),
+                    new Player(9, "Haley", "haleyImage")
+                ),
+                new Player(4, "Harvey", "harveyImage"),
+            };
+            yield return new object[] {
+                true,
+                new Player[]
+                {
+                    new Player(9, "Franck", "franckImage"),
+                    new Player(10, "Leo", "leoImage"),
+                    new Player(11, "Marnie", "marnieImage"),
+                    new Player(15, "Willy", "willyImage"),
+                    new Player(50, "Elvis", "elvisImage"),
+                },
+                new PlayerManager(
+                    new Player(10, "Leo", "leoImage"),
+                    new Player(11, "Marnie", "marnieImage"),
+                    new Player(15, "Willy", "willyImage"),
+                    new Player(50, "Elvis", "elvisImage")
+                ),
+                new Player(9, "Franck", "franckImage"),
+            };
+            yield return new object[] {
+                false,
+                new Player[]
+                {
+                    new Player(45, "Emily", "emilyImage"),
+                    new Player(25, "Leah", "leahImage"),
+                    new Player(22, "Gus", "gusImage"),
+                    new Player(71, "Clint", "clintImage"),
+                    new Player(99, "Sebastian", "sebastianImage"),
+                },
+                new PlayerManager(
+                    new Player(45, "Emily", "emilyImage"),
+                    new Player(25, "Leah", "leahImage"),
+                    new Player(22, "Gus", "gusImage"),
+                    new Player(71, "Clint", "clintImage"),
+                    new Player(99, "Sebastian", "sebastianImage")
+                ),
+                new Player(22, "Gus", "gusImage"),
+            };
+            yield return new object[] {
+                false,
+                new Player[]
+                {
+                    new Player(12, "Marlon", "marlonImage"),
+                    new Player(45, "Vincent", "vincentImage"),
+                    new Player(85, "Jas", "jasImage"),
+                    new Player(75, "Pierre", "pierreImage"),
+                },
+                new PlayerManager(
+                    new Player(12, "Marlon", "marlonImage"),
+                    new Player(45, "Vincent", "vincentImage"),
+                    new Player(85, "Jas", "jasImage"),
+                    new Player(75, "Pierre", "pierreImage"),
+                    new Player(41, "Demetrius", "demetriusImage"),
+                    new Player(43, "Maru", "maruImage")
+                ),
+                new Player(45, "Vincent", "vincentImage"),
+            };
+        }
+    }
+}
