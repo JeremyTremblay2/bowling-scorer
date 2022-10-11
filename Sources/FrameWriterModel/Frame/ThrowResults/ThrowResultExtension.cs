296046b6f869082fbdfb2274f6d9150aa03c0c5b
jeremy.tremblay <jeremy.tremblay@etu.uca.fr>
2022-10-11 09:32:17 +0200

    [#53] Add more conversion method in the ThrowResult class

--- Sources/FrameWriterModel/Frame/ThrowResults/ThrowResultExtension.cs
+++ Sources/FrameWriterModel/Frame/ThrowResults/ThrowResultExtension.cs
@@ -13,7 +13,7 @@ namespace FrameWriterModel.Frame.ThrowResults
     {
         public static int ToInt(this ThrowResult throwResult)
         {
-            return (throwResult switch
+            return throwResult switch
             {
                 ThrowResult.NONE => 0,
                 ThrowResult.ZERO => 0,
@@ -28,8 +28,48 @@ namespace FrameWriterModel.Frame.ThrowResults
                 ThrowResult.NINE => 9,
                 ThrowResult.SPARE => 10,
                 ThrowResult.STRIKE => 10,
-                _ => throw new ArgumentException("Cannot convert this ThrowResult to int : " + throwResult)
-            });
+                _ => throw new ArgumentException($"Cannot convert this ThrowResult to int : {throwResult}")
+            };
+        }
+
+        public static ThrowResult ToThrowResult(this int throwResult, bool isStrike)
+        {
+            return throwResult switch
+            {
+                0 => ThrowResult.ZERO,
+                1 => ThrowResult.ONE,
+                2 => ThrowResult.TWO,
+                3 => ThrowResult.TREE,
+                4 => ThrowResult.FOUR,
+                5 => ThrowResult.FIVE,
+                6 => ThrowResult.SIX,
+                7 => ThrowResult.SEVEN,
+                8 => ThrowResult.EIGHT,
+                9 => ThrowResult.NINE,
+                10 => isStrike == true ? ThrowResult.STRIKE : ThrowResult.SPARE,
+                _ => throw new ArgumentException($"Cannot convert this int to ThrowResult : {throwResult}")
+            };
+        }
+
+        public static char ToChar(this ThrowResult throwResult)
+        {
+            return throwResult switch
+            {
+                ThrowResult.NONE => '-',
+                ThrowResult.ZERO => '0',
+                ThrowResult.ONE => '1',
+                ThrowResult.TWO => '2',
+                ThrowResult.TREE => '3',
+                ThrowResult.FOUR => '4',
+                ThrowResult.FIVE => '5',
+                ThrowResult.SIX => '6',
+                ThrowResult.SEVEN => '7',
+                ThrowResult.EIGHT => '8',
+                ThrowResult.NINE => '9',
+                ThrowResult.SPARE => '/',
+                ThrowResult.STRIKE => 'X',
+                _ => throw new ArgumentException($"Cannot convert this ThrowResult to char : {throwResult}")
+            };
         }
     }
 }
