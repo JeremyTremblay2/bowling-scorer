e214770178882a2a7e0469b3d92f1787a591b183
mickael.dubois <Mickael.DUBOIS@etu.uca.fr>
2022-10-11 14:08:40 +0200

    [#49] Added some documentation

--- Sources/Model/Score/Rules/ClassicRules.cs
+++ Sources/Model/Score/Rules/ClassicRules.cs
@@ -12,10 +12,23 @@ namespace Model.Score.Rules
 {
     public class ClassicRules : ARules
     {
+        /// <summary>
+        /// Use a classic calculator and classicWriters to apply classic game rules
+        /// </summary>
+        /// <param name="scoreCalculator"></param>
+        /// <param name="writers"></param>
         public ClassicRules() : base(new ClassicScoreCalculator(), new List<AFrameWriter> { new ClassicFrameWriter(), new ClassicLastFrameWriter()})
         {
         }
 
+        /// <summary>
+        /// Write a value in the given AFrame by using the writers.
+        /// Here if a ClassicFrame is given, a ClassicFrameWriter will be used to write the result
+        /// if a ClassicLastFrame is given, a ClassicLastFrameWriter will be used to write the result
+        /// </summary>
+        /// <param name="frame"></param>
+        /// <param name="index"></param>
+        /// <param name="throwResult"></param>
         public override void WriteValue(AFrame frame, int index, ThrowResult throwResult)
         {
             ClassicFrame classicFrame = frame as ClassicFrame;
