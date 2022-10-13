283a7fa956ef29759c2982d3e1e8948feb5bd0c7
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-13 18:39:05 +0200

    [#60] Add the data test class for the unit test.

---
+++ Sources/Tests/UnitTests/Score/Rules/ClassicFullScoreTableDetectorDataTest.cs
@@ -0,0 +1,565 @@
+﻿using FrameWriterModel.Frame.ThrowResults;
+using Model.Score;
+using Model.Score.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Score.Rules
+{
+    /// <summary>
+    /// Represent data for unit testing the ClassicFullScoreTableDetector class.
+    /// </summary>
+    public class ClassicFullScoreTableDetectorDataTest
+    {
+        public static IEnumerable<object[]> Data_ScoreTables()
+        {
+            // Insert a correct score table.
+            yield return new object[] {
+                true,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.STRIKE,
+                        ThrowResult.STRIKE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the second frame which is empty.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.NONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.STRIKE,
+                        ThrowResult.STRIKE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the third frame which has a NONE value in the first box.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.STRIKE,
+                        ThrowResult.STRIKE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the third frame which has a NONE value in the second box.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.NONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.STRIKE,
+                        ThrowResult.STRIKE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the last frame which is empty.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.NONE,
+                        ThrowResult.NONE,
+                    },
+                }),
+            };
+
+            // Incorrect because of the last frame which has first box at NONE.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the last frame which has second box at NONE.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.STRIKE,
+                        ThrowResult.NONE,
+                        ThrowResult.TWO,
+                    },
+                }),
+            };
+
+            // Incorrect because of the last frame which has third box at NONE but it has a STRIKE.
+            yield return new object[] {
+                false,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.STRIKE,
+                        ThrowResult.NONE,
+                    },
+                }),
+            };
+
+            // A correct configuration with a spare at the last frame.
+            yield return new object[] {
+                true,
+                ToScoreTable(new ThrowResult[][]
+                {
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.ZERO,
+                        ThrowResult.EIGHT,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.TWO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NONE,
+                        ThrowResult.STRIKE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.TWO,
+                        ThrowResult.SEVEN,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.NINE,
+                        ThrowResult.SPARE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.FOUR,
+                        ThrowResult.THREE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.SEVEN,
+                        ThrowResult.ONE,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.EIGHT,
+                        ThrowResult.ZERO,
+                    },
+                    new ThrowResult[]
+                    {
+                        ThrowResult.THREE,
+                        ThrowResult.SPARE,
+                        ThrowResult.FIVE,
+                    },
+                }),
+            };
+        }
+
+        private static ScoreTable ToScoreTable(ThrowResult[][] throwResults)
+        {
+            ScoreTable scoreTable = new(new ClassicRules());
+            for (int i = 0; i < throwResults.Length; i++)
+            {
+                var currentFrame = throwResults[i];
+                for (int j = 0; j < currentFrame.Length; j++)
+                {
+                    scoreTable.WriteValue(i, j, throwResults[i][j]);
+                }
+            }
+            return scoreTable;
+        }
+    }
+}
