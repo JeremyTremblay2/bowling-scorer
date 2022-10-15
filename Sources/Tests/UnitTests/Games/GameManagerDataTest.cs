c97a45b6d3f248cd943db6bdc61430772b26719d
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-15 19:22:58 +0200

    [#35] Add unit tests for the GameManager.

---
+++ Sources/Tests/UnitTests/Games/GameManagerDataTest.cs
@@ -0,0 +1,242 @@
+﻿using Model.Games;
+using Model.Players;
+using Model.Score;
+using Model.Score.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.Games
+{
+    public class GameManagerDataTest
+    {
+        public static Game gameFinished = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(12, "Maya", "beeImage"), GameDataTest.scoreTableComplete },
+            { new Player(52, "Toto", "totoImage"), GameDataTest.anotherScoreTableComplete },
+        }, true, 23);
+
+        public static Game gameNotFinished = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(12, "Maya", "beeImage"), GameDataTest.scoreTableComplete },
+        }, false, 12);
+
+        public static Game gameNotFinished2 = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(12, "Maya", "beeImage"), GameDataTest.scoreTableComplete },
+            { new Player(52, "Toto", "totoImage"), GameDataTest.scoreTableNotComplete },
+        }, false, 12);
+
+        
+        public static IEnumerable<object[]> Data_RemoveGames()
+        {
+            GameManager manager1 = new GameManager();
+            manager1.AddGame(gameFinished);
+
+            GameManager manager2 = new GameManager();
+            manager2.AddGame(gameFinished);
+            manager2.AddGame(gameNotFinished);
+
+            GameManager manager3 = new GameManager();
+            manager3.AddGame(gameFinished);
+            manager3.AddGame(gameNotFinished);
+
+            yield return new object[]
+            {
+                true,
+                true,
+                manager1,
+                gameFinished.ID
+            };
+
+            yield return new object[]
+            {
+                true,
+                false,
+                manager2,
+                gameFinished.ID
+            };
+
+            yield return new object[]
+            {
+                true,
+                true,
+                manager2,
+                gameNotFinished.ID
+            };
+
+            yield return new object[]
+            {
+                false,
+                false,
+                manager3,
+                17
+            };
+        }
+
+        public static IEnumerable<object[]> Data_AddGames()
+        {
+            yield return new object[]
+            {
+                true,
+                false,
+                0,
+                true,
+                1,
+                new Game[]
+                {
+                    gameFinished
+                },
+                new GameManager()
+            };
+
+            yield return new object[]
+            {
+                true,
+                false,
+                0,
+                false,
+                1,
+                new Game[]
+                {
+                    gameNotFinished
+                },
+                new GameManager()
+            };
+
+            yield return new object[]
+            {
+                true,
+                false,
+                0,
+                false,
+                1,
+                new Game[]
+                {
+                    gameNotFinished2
+                },
+                new GameManager()
+            };
+
+            yield return new object[]
+            {
+                false,
+                true,
+                1,
+                false,
+                2,
+                new Game[]
+                {
+                    gameNotFinished,
+                    gameNotFinished2
+                },
+                new GameManager()
+            };
+
+            yield return new object[]
+            {
+                true,
+                false,
+                0,
+                false,
+                2,
+                new Game[]
+                {
+                    gameFinished,
+                    gameNotFinished2
+                },
+                new GameManager()
+            };
+        }
+
+        public static IEnumerable<object[]> Data_Equals()
+        {
+            GameManager manager1 = new GameManager();
+            GameManager manager2 = new GameManager();
+            GameManager manager3 = new GameManager();
+            GameManager manager4 = new GameManager();
+
+            manager2.AddGame(gameFinished);
+            manager3.AddGame(gameNotFinished);
+            manager4.AddGame(gameNotFinished);
+
+            yield return new object[]
+            {
+                true,
+                manager1,
+                manager1
+            };
+
+            yield return new object[]
+            {
+                true,
+                manager2,
+                manager2
+            };
+
+            yield return new object[]
+            {
+                true,
+                manager3,
+                manager3
+            };
+
+            yield return new object[]
+            {
+                false,
+                manager1,
+                null
+            };
+
+            yield return new object[]
+            {
+                false,
+                null,
+                manager1
+            };
+
+            yield return new object[]
+            {
+                false,
+                12,
+                manager1
+            };
+
+            yield return new object[]
+            {
+                false,
+                manager1,
+                23
+            };
+
+            yield return new object[]
+            {
+                false,
+                manager1,
+                manager2,
+            };
+
+            yield return new object[]
+            {
+                false,
+                manager1,
+                manager3,
+            };
+
+            yield return new object[]
+            {
+                false,
+                manager2,
+                manager3,
+            };
+            yield return new object[]
+            {
+                true,
+                manager3,
+                manager4,
+            };
+        }
+    }
+}
---
+++ Sources/Tests/UnitTests/Games/GameManager_UT.cs
@@ -0,0 +1,107 @@
+﻿using Model.Games;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.Games
+{
+    public class GameManager_UT
+    {
+        [Fact]
+        public void CreateGameManagerShouldInitializeCollection()
+        {
+            GameManager gameManager = new();
+            Assert.NotNull(gameManager.Games);
+        }
+
+        [Fact]
+        public void CreateGameManagerShouldCreateEmptyCollection()
+        {
+            GameManager gameManager = new();
+            Assert.Empty(gameManager.Games);
+        }
+
+        [Fact]
+        public void CreateGameManagerShouldCreateReadOnlyCollection()
+        {
+            GameManager gameManager = new();
+            Assert.Equal(typeof(ReadOnlyCollection<Game>), gameManager.Games.GetType());
+        }
+
+        [Fact]
+        public void CreateGameManagerShouldInitializeCurrentGame()
+        {
+            GameManager gameManager = new();
+            Assert.Null(gameManager.CurrentGame);
+        }
+
+        [Theory]
+        [MemberData(nameof(GameManagerDataTest.Data_AddGames), MemberType = typeof(GameManagerDataTest))]
+        public void AddGamesToGameManagerShouldAddGames(bool expectedResult,
+                                                        bool expectedInvalidOperationException,
+                                                        int indexExpectedToFail,
+                                                        bool expectedCurrentGameNull,
+                                                        int expectedNumberOfGames,
+                                                        Game[] gamesToAdd,
+                                                        GameManager manager)
+        {
+            bool result = false;
+            for (int i = 0; i < gamesToAdd.Count(); i++)
+            {
+                if (expectedInvalidOperationException && i == indexExpectedToFail)
+                {
+                    Assert.Throws<InvalidOperationException>(() => manager.AddGame(gamesToAdd[i]));
+                }
+                else
+                {
+                    result = manager.AddGame(gamesToAdd[i]);
+                }
+            }
+            if (!expectedInvalidOperationException)
+            {
+                Assert.Equal(expectedResult, result);
+                Assert.Equal(expectedNumberOfGames, manager.Games.Count);
+                Assert.Equal(expectedCurrentGameNull, manager.CurrentGame == null);
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(GameManagerDataTest.Data_RemoveGames), MemberType = typeof(GameManagerDataTest))]
+        public void RemoveGameShouldRemoveGameFromCollection(bool expectedResult,
+                                                             bool expectedCurrentGameNull,
+                                                             GameManager manager,
+                                                             int ID)
+        {
+            bool result = manager.RemoveGame(ID);
+            Assert.Equal(expectedResult, result);
+            Assert.Equal(expectedCurrentGameNull, manager.CurrentGame == null);
+        }
+
+        [Fact]
+        public void ChangeStatusCurrentGameShouldFireTheEventAndSetToNullTheCurrentGame()
+        {
+            GameManager manager = new GameManager();
+            manager.AddGame(GameManagerDataTest.gameNotFinished);
+            Assert.NotNull(manager.CurrentGame);
+            Assert.Single(manager.Games);
+            manager.CurrentGame.Finish();
+            Assert.Null(manager.CurrentGame);
+            Assert.Single(manager.Games);
+        }
+
+        [Theory]
+        [MemberData(nameof(GameManagerDataTest.Data_Equals), MemberType = typeof(GameManagerDataTest))]
+        public void EqualsShouldReturnsLogicalValue(bool expectedResult,
+                                                    object manager1,
+                                                    object manager2)
+        {
+            if (manager1 != null) Assert.Equal(expectedResult, manager1.Equals(manager2));
+            if (manager2 != null) Assert.Equal(expectedResult, manager2.Equals(manager1));
+            if (manager1 is GameManager && manager2 is GameManager && expectedResult) Assert.Equal(manager1.GetHashCode(), manager2.GetHashCode());
+        }
+    }
+}
