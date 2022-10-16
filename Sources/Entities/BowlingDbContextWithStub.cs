94e41860008f4bb35aebbda8df7c02d2a3aa8275
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-17 01:40:48 +0200

    [#85] Add a stub DBContext with a fonctionnal test.

---
+++ Sources/Entities/BowlingDbContextWithStub.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BowlingDbContextWithStub : BowlingDbContext
+    {
+        public BowlingDbContextWithStub()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<PlayerEntity>().HasData(
+                new PlayerEntity { ID = 1, Name = "Mickael", Image = "imageMickael.png" },
+                new PlayerEntity { ID = 2, Name = "Jeremy", Image = "imageJeremy.png" },
+                new PlayerEntity { ID = 3, Name = "Lucas", Image = "imageLucas.png" }
+            );
+        }
+    }
+}
