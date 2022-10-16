dff2c70f5dc3e857b8a68eb21cfeb76565c1a5a5
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-17 01:56:58 +0200

    [#85] Add the last documentation paragraphs on the classes.

--- Sources/Entities/BowlingDbContextWithStub.cs
+++ Sources/Entities/BowlingDbContextWithStub.cs
@@ -8,13 +8,25 @@ using System.Threading.Tasks;
 
 namespace Entities
 {
+    /// <summary>
+    /// Represent a DBcontext class for our BowlingScorer app that will also contains some data when initialized.
+    /// </summary>
     public class BowlingDbContextWithStub : BowlingDbContext
     {
+        /// <summary>
+        /// Create a new instance of BowlingDbContextWithStub.
+        /// Ensure that the database is created.
+        /// </summary>
         public BowlingDbContextWithStub()
         {
             Database.EnsureCreated();
         }
 
+        /// <summary>
+        /// Method call when the model is creating, and allow us to define some settings and constraints 
+        /// about our entities before creating them.
+        /// </summary>
+        /// <param name="modelBuilder">The model builder.</param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
