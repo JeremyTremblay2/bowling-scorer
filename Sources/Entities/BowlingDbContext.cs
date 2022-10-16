730049de4d25a425341a1a0dd29cd7d2cfd156e5
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-17 01:39:24 +0200

    [#85] Add the DBcontext class with the player entities.

---
+++ Sources/Entities/BowlingDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class BowlingDbContext : DbContext
+    {
+        public DbSet<PlayerEntity> Players { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source=Entities.BowlingDatabase.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerEntity>().ToTable("Player");
+
+            modelBuilder.Entity<PlayerEntity>().Property(n => n.Name)
+                                               .IsRequired()
+                                               .HasMaxLength(256);
+
+            modelBuilder.Entity<PlayerEntity>().Property(n => n.ID)
+                                               .ValueGeneratedOnAdd();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
