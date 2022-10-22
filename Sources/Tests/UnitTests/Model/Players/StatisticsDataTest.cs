2177b977b9ec098021f3349190d71454acc65860
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-22 16:19:55 +0200

    [#75] Add uni tests on the Statistics class.

---
+++ Sources/Tests/UnitTests/Model/Players/StatisticsDataTest.cs
@@ -0,0 +1,343 @@
+﻿using Model.Games;
+using Model.Players;
+using Model.Score.Rules;
+using Model.Score;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.Games;
+
+namespace UnitTests.Model.Players
+{
+    public class StatisticsDataTest
+    {
+        // Winner: Clint, Best score: 104
+        public static Game gameFinished = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(21, "Clint", "clintImage"), GameDataTest.scoreTableComplete },
+            { new Player(14, "Haley", "haleyImage"), GameDataTest.anotherScoreTableComplete },
+        }, true, 23);
+
+        // Winner: Haley, Best score: 104
+        public static Game gameFinished2 = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(21, "Clint", "clintImage"), GameDataTest.anotherScoreTableComplete },
+            { new Player(14, "Haley", "haleyImage"), GameDataTest.scoreTableComplete },
+        }, true, 36);
+
+        public static Game gameNotFinished = new Game(new ClassicRules(), new Dictionary<Player, ScoreTable>
+        {
+            { new Player(14, "Haley", "haleyImage"), GameDataTest.scoreTableComplete },
+        }, false, 12);
+
+        public static Player playerEmpty = new Player(18, "Abigail", "abiImage");
+
+        public static Player player83 = new Player(14, "Gus", "gusImage");
+
+        public static Player player104 = new Player(21, "Clint", "clintImage");
+
+        static StatisticsDataTest() {
+            player104.AddGame(gameFinished);
+            player83.AddGame(gameFinished);
+        }
+
+        public static IEnumerable<object[]> Test_StatisticsDataAddGame()
+        {
+            Player player83 = new Player(14, "Gus", "gusImage");
+            Player player104 = new Player(21, "Clint", "clintImage");
+            player104.AddGame(gameFinished);
+            player83.AddGame(gameFinished);
+            yield return new object[] {
+                true,
+                1,
+                1,
+                0,
+                104,
+                20,
+                5.0,
+                104,
+                new Player(21, "Clint", "clintImage"),
+                new Game[]
+                {
+                    gameFinished
+                }
+            };
+
+            yield return new object[] {
+                true,
+                1,
+                0,
+                1,
+                83,
+                20,
+                4.4,
+                83,
+                new Player(14, "Haley", "haleyImage"),
+                new Game[]
+                {
+                    gameFinished
+                }
+            };
+
+            yield return new object[] {
+                true,
+                2,
+                1,
+                1,
+                104,
+                40,
+                4.7,
+                93.5,
+                player83,
+                new Game[]
+                {
+                    gameFinished2
+                }
+            };
+
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                new Player(14, "Haley", "haleyImage"),
+                new Game[]
+                {
+                    gameNotFinished
+                }
+            };
+
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                new Player(14, "Haley", "haleyImage"),
+                new Game[]
+                {
+                    null
+                }
+            };
+
+            // Non existing player
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                new Player(15, "David", "davidImage"),
+                new Game[]
+                {
+                    gameNotFinished
+                }
+            };
+
+            yield return new object[] {
+                false,
+                1,
+                1,
+                0,
+                104,
+                20,
+                5.0,
+                104,
+                player104,
+                new Game[]
+                {
+                    gameFinished,
+                }
+            };
+        }
+
+        public static IEnumerable<object[]> Test_StatisticsRemoveAddGame()
+        {
+            Player player83 = new Player(14, "Gus", "gusImage");
+            Player player104 = new Player(21, "Clint", "clintImage");
+            player104.AddGame(gameFinished);
+            player83.AddGame(gameFinished);
+            player83.AddGame(gameFinished2);
+            yield return new object[] {
+                true,
+                1,
+                0,
+                1,
+                83,
+                20,
+                4.4,
+                83,
+                player83,
+                new Game[]
+                {
+                    gameFinished2
+                }
+            };
+
+            yield return new object[] {
+                true,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                player104,
+                new Game[]
+                {
+                    gameFinished
+                }
+            };
+
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                player104,
+                new Game[]
+                {
+                    null
+                }
+            };
+
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                new Player(2, "Toto", "totoImage"),
+                new Game[]
+                {
+                    null
+                }
+            };
+
+            yield return new object[] {
+                false,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                0,
+                player104,
+                new Game[]
+                {
+                    gameNotFinished
+                }
+            };
+        }
+
+
+        public static IEnumerable<object[]> Test_StatisticsDataEquality()
+        {
+            Player playerEmpty = new Player(18, "Abigail", "abiImage");
+            Player playerEmpty2 = new Player(18, "Abigail", "abiImage");
+            Player player83 = new Player(14, "Gus", "gusImage");
+            Player player832 = new Player(14, "Gus", "gusImage");
+            Player player104 = new Player(21, "Clint", "clintImage");
+            player104.AddGame(gameFinished);
+            player83.AddGame(gameFinished);
+            player832.AddGame(gameFinished);
+
+            yield return new object[] {
+                true,
+                playerEmpty.Statistics,
+                playerEmpty.Statistics
+            };
+
+            yield return new object[] {
+                true,
+                player83.Statistics,
+                player83.Statistics
+            };
+
+            yield return new object[] {
+                true,
+                playerEmpty.Statistics,
+                playerEmpty2.Statistics
+            };
+
+            yield return new object[] {
+                true,
+                player83.Statistics,
+                player832.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player104.Statistics,
+                player832.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player832.Statistics,
+                playerEmpty.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player83.Statistics,
+                playerEmpty.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player832.Statistics,
+                player104.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player104.Statistics,
+                playerEmpty.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                null,
+                playerEmpty.Statistics
+            };
+
+            yield return new object[] {
+                false,
+                player104.Statistics,
+                null
+            };
+
+            yield return new object[] {
+                true,
+                null,
+                null
+            };
+        }
+
+
+
+
+    }
+}
--- Sources/Tests/UnitTests/Model/Players/Statistics_UT.cs
+++ Sources/Tests/UnitTests/Model/Players/Statistics_UT.cs
@@ -1,4 +1,8 @@
-﻿using Model.Players;
+﻿using Model.Games;
+using Model.Players;
+using System;
+using System.Linq;
+using UnitTests.Model.Players;
 using Xunit;
 
 namespace UnitTests.Players
@@ -14,16 +18,22 @@ namespace UnitTests.Players
             Statistics statistics = new();
             Assert.NotNull(statistics.ThrowResults);
             Assert.NotNull(statistics.Scores);
-            Assert.NotNull(statistics.GamesID);
+            Assert.NotNull(statistics.Games);
         }
 
         [Fact]
-        public void StatisticsShouldBeEmptyWhenInitialized()
+        public void CollectionsShouldBeEmptyWhenInitialized()
         {
             Statistics statistics = new();
-            Assert.Empty(statistics.ThrowResults);
+            Assert.NotEmpty(statistics.ThrowResults);
             Assert.Empty(statistics.Scores);
-            Assert.Empty(statistics.GamesID);
+            Assert.Empty(statistics.Games);
+        }
+
+        [Fact]
+        public void StatisticsShouldBeEqualToZeroWhenInitialized()
+        {
+            Statistics statistics = new();
             Assert.Equal(0, statistics.BestScore);
             Assert.Equal(0, statistics.NumberOfGames);
             Assert.Equal(0, statistics.NumberOfDefeat);
@@ -31,20 +41,105 @@ namespace UnitTests.Players
         }
 
         [Fact]
-        public void AddGameShouldIncrementNumberOfGames()
+        public void CalcultatedPropertiesShouldReturnsZeroWhenInitialized()
         {
             Statistics statistics = new();
-            statistics.AddGame(21);
-            Assert.Equal(1, statistics.NumberOfGames);
+            Assert.Equal(0, statistics.MediumScore);
+            Assert.Equal(0, statistics.MediumScore);
+        }
+
+        [Theory]
+        [MemberData(nameof(StatisticsDataTest.Test_StatisticsDataAddGame), MemberType = typeof(StatisticsDataTest))]
+        public void AddGameShouldUpdateStatistics(bool expectedLastResult,
+                                                  int expectedNumberOfGames,
+                                                  int expectedNumberOfVictories,
+                                                  int expectedNumberOfDefeat,
+                                                  int expectedBestScore,
+                                                  int expectedNumberOfThrowResults,
+                                                  double expectedMediumThrow,
+                                                  double expectedMediumScore,
+                                                  Player player,
+                                                  Game[] gamesToAdd)
+        {
+            bool result = false;
+            foreach (Game game in gamesToAdd)
+            {
+                result = player.AddGame(game);
+            }
+            Assert.Equal(expectedLastResult, result);
+            Assert.Equal(expectedNumberOfGames, player.Statistics.Games.Count);
+            Assert.Equal(expectedNumberOfVictories, player.Statistics.NumberOfVictory);
+            Assert.Equal(expectedNumberOfDefeat, player.Statistics.NumberOfDefeat);
+            Assert.Equal(expectedBestScore, player.Statistics.BestScore);
+            Assert.Equal(expectedNumberOfThrowResults, player.Statistics.ThrowResults.Values.Sum());
+            Assert.Equal(expectedMediumThrow, Math.Round(player.Statistics.MediumThrow, 1));
+            Assert.Equal(expectedMediumScore, Math.Round(player.Statistics.MediumScore, 1));
+        }
+
+        [Theory]
+        [MemberData(nameof(StatisticsDataTest.Test_StatisticsRemoveAddGame), MemberType = typeof(StatisticsDataTest))]
+        public void RemoveGameShouldUpdateStatistics(bool expectedLastResult,
+                                                     int expectedNumberOfGames,
+                                                     int expectedNumberOfVictories,
+                                                     int expectedNumberOfDefeat,
+                                                     int expectedBestScore,
+                                                     int expectedNumberOfThrowResults,
+                                                     double expectedMediumThrow,
+                                                     double expectedMediumScore,
+                                                     Player player,
+                                                     Game[] gamesToRemove)
+        {
+            bool result = false;
+            foreach (Game game in gamesToRemove)
+            {
+                result = player.RemoveGame(game);
+            }
+            Assert.Equal(expectedLastResult, result);
+            Assert.Equal(expectedNumberOfGames, player.Statistics.Games.Count);
+            Assert.Equal(expectedNumberOfVictories, player.Statistics.NumberOfVictory);
+            Assert.Equal(expectedNumberOfDefeat, player.Statistics.NumberOfDefeat);
+            Assert.Equal(expectedBestScore, player.Statistics.BestScore);
+            Assert.Equal(expectedNumberOfThrowResults, player.Statistics.ThrowResults.Values.Sum());
+            Assert.Equal(expectedMediumThrow, Math.Round(player.Statistics.MediumThrow, 1));
+            Assert.Equal(expectedMediumScore, Math.Round(player.Statistics.MediumScore, 1));
+        }
+
+        [Theory]
+        [MemberData(nameof(StatisticsDataTest.Test_StatisticsDataEquality), MemberType = typeof(StatisticsDataTest))]
+        public void EqualsAndHashCodeShouldReturnLogicalValues(bool expectedResult, 
+                                                               Statistics statistics1, 
+                                                               Statistics statistics2)
+        {
+            Assert.Equal(expectedResult, statistics1 == statistics2);
+            Assert.Equal(!expectedResult, statistics1 != statistics2);
+            if (statistics1 != null) Assert.Equal(expectedResult, statistics1.Equals(statistics2));
+            if (statistics2 != null) Assert.Equal(expectedResult, statistics2.Equals(statistics1));
+            if (statistics1 != null && statistics2 != null) {
+                Assert.Equal(expectedResult, statistics1.GetHashCode().Equals(statistics2.GetHashCode()));
+            }
         }
 
         [Fact]
-        public void RemoveGameShouldDecrementNumberOfGames()
+        public void ComparisonOperatorsShouldReturnLogicValues()
         {
-            Statistics statistics = new();
-            statistics.AddGame(21);
-            statistics.RemoveGame(21);
-            Assert.Equal(0, statistics.NumberOfGames);
+            Statistics statistics1 = StatisticsDataTest.playerEmpty.Statistics;
+            Statistics statistics2 = StatisticsDataTest.player83.Statistics;
+            Statistics statistics3 = StatisticsDataTest.player104.Statistics;
+            Assert.True(statistics1 < statistics2);
+            Assert.True(statistics1 <= statistics2);
+            Assert.True(statistics2 > statistics1);
+            Assert.True(statistics2 >= statistics1);
+            Assert.True(statistics3 > statistics2);
+            Assert.True(statistics3 >= statistics2);
+            Assert.True(statistics3 >= statistics1);
+        }
+
+        [Fact]
+        public void EqualityOperatorShouldReturnsNullWhenEqualsNullValue()
+        {
+            Statistics Statistics1 = null;
+            Statistics Statistics2 = null;
+            Assert.True(Statistics1 == Statistics2);
         }
     }
 }
