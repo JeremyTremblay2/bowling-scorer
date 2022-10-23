f0230a3ef8130e3175008defe4cc4bf7d2889937
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-23 14:55:07 +0200

    [#93] Add generic mapper and interface for our entities.

--- Sources/Entity2Model/Mapper/IMapper.cs
+++ Sources/Entity2Model/Mapper/IMapper.cs
@@ -6,12 +6,38 @@ using System.Threading.Tasks;
 
 namespace Entity2Model.Mapper
 {
+    /// <summary>
+    /// Defines a contract that the implementing class must provide methods that allow data mapping. 
+    /// Represents a generic two-parameter mapper.
+    /// </summary>
+    /// <typeparam name="T">The first parameter to map.</typeparam>
+    /// <typeparam name="P">The second parameter to map.</typeparam>
     public interface IMapper<T, P>
     {
+        /// <summary>
+        /// Map the two elements given in parameter.
+        /// </summary>
+        /// <param name="elem1">The first telement to map.</param>
+        /// <param name="elem2">The second element to map.</param>
         void Map(T elem1, P elem2);
 
-        T? Get(P entity);
+        /// <summary>
+        /// Get The first element from the second if it already exists in the mapper.
+        /// </summary>
+        /// <param name="elem">The first element.</param>
+        /// <returns>The second element corresponding to the first element.</returns>
+        T? Get(P elem);
 
-        P? Get(T entity);
+        /// <summary>
+        /// Get The second element from the first if it already exists in the mapper. 
+        /// </summary>
+        /// <param name="elem">The second element.</param>
+        /// <returns>The first element corresponding to the second element.</returns>
+        P? Get(T elem);
+
+        /// <summary>
+        /// Clear the mapper.
+        /// </summary>
+        void Clear();
     }
 }
