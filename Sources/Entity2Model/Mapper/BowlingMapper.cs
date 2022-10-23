3e053a3901a684a7e4ad41cb0daf127c51d10c54
Jérémy TREMBLAY <jeremy.tremblay@etu.uca.fr>
2022-10-23 14:55:40 +0200

    [#93] Add a specific mapper for our players.

--- Sources/Entity2Model/Mapper/BowlingMapper.cs
+++ Sources/Entity2Model/Mapper/BowlingMapper.cs
@@ -4,8 +4,14 @@ using Model.Players;
 
 namespace Entity2Model.Mapper
 {
-    public class BowlingMapper
+    /// <summary>
+    /// Class containing all the mappers used in our Bowling Scorer app.
+    /// </summary>
+    public static class BowlingMapper
     {
+        /// <summary>
+        /// Mapper used to do the correspondance between player entities and pleyr models.
+        /// </summary>
         public static IMapper<PlayerEntity, Player> Players = new Mapper<PlayerEntity, Player>();
     }
 }
